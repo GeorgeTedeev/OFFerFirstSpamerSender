2aa835c643f2fb024ccda38da1a6b9ce9b5f7226
GeorgeTedeev <biruang123@gmail.com>
2020-09-08 00:25:56 +0300

    after init

---
+++ src/main/java/com/project/offer/firstspamertasker/services/ScheduledService.java
@@ -0,0 +1,21 @@
+package com.project.offer.firstspamertasker.services;
+
+import org.springframework.stereotype.Service;
+import java.util.concurrent.ScheduledExecutorService;
+
+@Service
+public class ScheduledService {
+
+    private ScheduledExecutorService scheduledExecutorService;
+
+    public ScheduledService() {
+    }
+
+    public ScheduledExecutorService getScheduledExecutorService() {
+        return scheduledExecutorService;
+    }
+
+    public void setScheduledExecutorService(ScheduledExecutorService scheduledExecutorService) {
+        this.scheduledExecutorService = scheduledExecutorService;
+    }
+}
